444a32f6ed9d5741400b2d48aca3effee27dc390
christol okeke <50752054+okekec@users.noreply.github.com>
2020-05-05 22:57:21 -0500

    Initial commit for ML python script

---
+++ ML Model/train_classifier.py
@@ -0,0 +1,213 @@
+# Import libraries
+import sys
+import pandas as pd
+import numpy as np
+from sqlalchemy import create_engine
+import re
+import pickle
+import nltk
+from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.model_selection import train_test_split
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from sklearn.pipeline import Pipeline, FeatureUnion
+from sklearn.model_selection import GridSearchCV
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, make_scorer
+import xgboost as xgb
+from xgboost import XGBClassifier
+nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
+import warnings
+warnings.simplefilter('ignore')
+
+
+def load_data(database_filepath):
+     """Load and merge messages and categories datasets
+    
+    Args:
+    database_filename: string. Filename for SQLite database containing cleaned message data.
+       
+    Returns:
+    X: dataframe. Dataframe containing features dataset.
+    y: dataframe. Dataframe containing labels dataset.
+    category_names: list of strings. List containing category names.
+    """
+    # Load data from database
+engine = create_engine('sqlite:///' + database_filepath)
+df = pd.read_sql("SELECT * FROM df", engine)
+    
+    # Create X and y datasets
+X = df['message']
+y = df.iloc[:, 4:]
+category_names = y.columns
+    return X, y, category_names
+
+def tokenize(text):
+    """
+    Tokenize function
+    
+    Arguments:
+        text -> list of text messages (english)
+    Output:
+        clean_tokens -> tokenized text, clean for ML modeling
+    """
+     # Convert text to lowercase and remove punctuation
+url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+detected_urls = re.findall(url_regex, text)
+for url in detected_urls:
+  text = text.replace(url, "urlplaceholder")
+
+tokens = word_tokenize(text) # Tokenize words
+lemmatizer = WordNetLemmatizer()
+
+clean_tokens = []
+for tok in tokens:
+    clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+    clean_tokens.append(clean_tok)
+    return clean_tokens
+
+# Define performance metric for used in the grid search 
+def performance_metric(y_true, y_pred):
+    """Calculate median F1 score for all of the output classifiers
+        Args:
+        y_true: array. Array containing actual labels.
+        y_pred: array. Array containing predicted labels.
+        Returns:
+        score: float. Median F1 score for all of the output classifiers
+        """
+    f1_list = []
+    for i in range(np.shape(y_pred)[1]):
+        f1 = f1_score(np.array(y_true)[:, i], y_pred[:, i])
+        f1_list.append(f1)
+        
+    score = np.median(f1_list)
+    return score
+
+def build_model():
+     """Build a machine learning pipeline
+    
+    Args:
+    None
+       
+    Returns:
+    cv: gridsearchcv object. Gridsearchcv object that transforms the data, creates the 
+    model object and finds the optimal model parameters.
+    """
+    # Create a pipeline to build the model
+pipeline = Pipeline([
+        ('vect', CountVectorizer(tokenizer = tokenize, min_df = 5)),
+        ('tfidf', TfidfTransformer(use_idf = True)),
+        ('clf', MultiOutputClassifier(RandomForestClassifier(n_estimators = 10,
+                                                             min_samples_split = 10)))
+    ])
+
+
+    # Created parameters for the classifier
+parameters = {'vect__max_df': [0.75, 1.0],
+                  'tfidf__use_idf':[True, False],
+                  'clf__estimator__n_estimators':[10, 25], 
+                  'clf__estimator__min_samples_split':[2, 5, 10]}
+    
+    
+scorer = make_scorer(performance_metric)
+    
+    # Created a grid search object
+cv = GridSearchCV(pipeline, param_grid = parameters, scoring = scorer, verbose = 10)
+return cv
+
+   
+def get_eval_metrics(actual, predicted, col_names):
+    """Calculate evaluation metrics for ML model
+    
+    Args:
+    actual: array. Array containing actual labels.
+    predicted: array. Array containing predicted labels.
+    col_names: list of strings. List containing names for each of the predicted fields.
+       
+    Returns:
+    metrics_df: dataframe. Dataframe containing the accuracy, precision, recall 
+    and f1 score for a given set of actual and predicted labels.
+    """
+    metrics = []
+    
+    # Calculate evaluation metrics for each set of labels
+    for i in range(len(col_names)):
+        accuracy = accuracy_score(actual[:, i], predicted[:, i])
+        precision = precision_score(actual[:, i], predicted[:, i])
+        recall = recall_score(actual[:, i], predicted[:, i])
+        f1 = f1_score(actual[:, i], predicted[:, i])
+        
+        metrics.append([accuracy, precision, recall, f1])
+    
+    # Create dataframe containing metrics
+    metrics = np.array(metrics)
+    metrics_df = pd.DataFrame(data = metrics, index = col_names, columns = ['Accuracy', 'Precision', 'Recall', 'F1'])
+      
+    return metrics_df
+
+def evaluate_model(model, X_test, y_test, category_names):
+    """Returns test accuracy, precision, recall and F1 score for fitted model
+    
+    Args:
+    model: model object. Fitted model object.
+    X_test: dataframe. Dataframe containing test features dataset.
+    y_test: dataframe. Dataframe containing test labels dataset.
+    category_names: list of strings. List containing category names.
+    
+    Returns:
+    None
+    """
+    # Predict labels for test dataset
+    y_pred = model.predict(X_test)
+    
+    # Calculate and print evaluation metrics
+    eval_metrics = get_eval_metrics(np.array(y_test), y_pred, category_names)
+    print(eval_metrics)
+
+
+def save_model(model, model_filepath):
+    """Pickle fitted model
+    
+    Args:
+    model: model object. Fitted model object.
+    model_filepath: string. Filepath for where fitted model should be saved
+    
+    Returns:
+    None
+    """
+    pickle.dump(model.best_estimator_, open(model_filepath, 'wb'))
+
+
+
+def main():
+    if len(sys.argv) == 3:
+        database_filepath, model_filepath = sys.argv[1:]
+        print('Loading data...\n    DATABASE: {}'.format(database_filepath))
+        X, y, category_names = load_data(database_filepath)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+        
+        print('Building model...')
+        model = build_model()
+        
+        print('Training model...')
+        model.fit(X_train, y_train)
+        
+        print('Evaluating model...')
+        evaluate_model(model, X_test, y_test, category_names)
+
+        print('Saving model...\n    MODEL: {}'.format(model_filepath))
+        save_model(model, model_filepath)
+
+        print('Trained model saved!')
+
+    else:
+        print('Please provide the filepath of the disaster messages database '\
+              'as the first argument and the filepath of the pickle file to '\
+              'save the model to as the second argument. \n\nExample: python '\
+              'train_classifier.py ../data/DisasterResponse.db classifier.pkl')
+
+
+if __name__ == '__main__':
+    main()
