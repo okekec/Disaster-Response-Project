dbc378a4a63d2b644c8eacabaac2f638de9f12d5
christol okeke <50752054+okekec@users.noreply.github.com>
2020-05-09 10:04:35 -0500

    Updated commit

--- ML Model/train_classifier.py
+++ ML Model/train_classifier.py
@@ -16,15 +16,15 @@ from nltk.stem import WordNetLemmatizer
 from sklearn.pipeline import Pipeline, FeatureUnion
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, make_scorer
-import xgboost as xgb
-from xgboost import XGBClassifier
+#import xgboost as xgb
+#from xgboost import XGBClassifier
 nltk.download(['punkt', 'wordnet', 'averaged_perceptron_tagger'])
 import warnings
 warnings.simplefilter('ignore')
 
 
 def load_data(database_filepath):
-     """Load and merge messages and categories datasets
+    """Load and merge messages and categories datasets
     
     Args:
     database_filename: string. Filename for SQLite database containing cleaned message data.
@@ -35,13 +35,13 @@ def load_data(database_filepath):
     category_names: list of strings. List containing category names.
     """
     # Load data from database
-engine = create_engine('sqlite:///' + database_filepath)
-df = pd.read_sql("SELECT * FROM df", engine)
-    
+    engine = create_engine('sqlite:///' + database_filepath)
+    df = pd.read_sql("SELECT * FROM df", engine)
+        
     # Create X and y datasets
-X = df['message']
-y = df.iloc[:, 4:]
-category_names = y.columns
+    X = df['message']
+    y = df.iloc[:, 4:]
+    category_names = y.columns
     return X, y, category_names
 
 def tokenize(text):
@@ -54,18 +54,18 @@ def tokenize(text):
         clean_tokens -> tokenized text, clean for ML modeling
     """
      # Convert text to lowercase and remove punctuation
-url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
-detected_urls = re.findall(url_regex, text)
-for url in detected_urls:
-  text = text.replace(url, "urlplaceholder")
+    url_regex = 'http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
+    detected_urls = re.findall(url_regex, text)
+    for url in detected_urls:
+        text = text.replace(url, "urlplaceholder")
 
-tokens = word_tokenize(text) # Tokenize words
-lemmatizer = WordNetLemmatizer()
+    tokens = word_tokenize(text) # Tokenize words
+    lemmatizer = WordNetLemmatizer()
 
-clean_tokens = []
-for tok in tokens:
-    clean_tok = lemmatizer.lemmatize(tok).lower().strip()
-    clean_tokens.append(clean_tok)
+    clean_tokens = []
+    for tok in tokens:
+        clean_tok = lemmatizer.lemmatize(tok).lower().strip()
+        clean_tokens.append(clean_tok)
     return clean_tokens
 
 # Define performance metric for used in the grid search 
@@ -79,14 +79,14 @@ def performance_metric(y_true, y_pred):
         """
     f1_list = []
     for i in range(np.shape(y_pred)[1]):
-        f1 = f1_score(np.array(y_true)[:, i], y_pred[:, i])
+        f1 = f1_score(np.array(y_true)[:, i], y_pred[:, i],average='micro')
         f1_list.append(f1)
         
     score = np.median(f1_list)
     return score
 
 def build_model():
-     """Build a machine learning pipeline
+    """Build a machine learning pipeline
     
     Args:
     None
@@ -96,26 +96,25 @@ def build_model():
     model object and finds the optimal model parameters.
     """
     # Create a pipeline to build the model
-pipeline = Pipeline([
+    pipeline = Pipeline([
         ('vect', CountVectorizer(tokenizer = tokenize, min_df = 5)),
         ('tfidf', TfidfTransformer(use_idf = True)),
-        ('clf', MultiOutputClassifier(RandomForestClassifier(n_estimators = 10,
-                                                             min_samples_split = 10)))
-    ])
+        ('clf', MultiOutputClassifier(RandomForestClassifier(n_estimators = 10, min_samples_split = 10)))
+        ])
 
 
     # Created parameters for the classifier
-parameters = {'vect__max_df': [0.75, 1.0],
-                  'tfidf__use_idf':[True, False],
-                  'clf__estimator__n_estimators':[10, 25], 
-                  'clf__estimator__min_samples_split':[2, 5, 10]}
+    parameters = {'vect__max_df': [0.75, 1.0],
+                    'tfidf__use_idf':[True, False],
+                    'clf__estimator__n_estimators':[10, 25], 
+                    'clf__estimator__min_samples_split':[2, 5, 10]}
+        
     
-    
-scorer = make_scorer(performance_metric)
+    scorer = make_scorer(performance_metric)
     
     # Created a grid search object
-cv = GridSearchCV(pipeline, param_grid = parameters, scoring = scorer, verbose = 10)
-return cv
+    cv = GridSearchCV(pipeline, param_grid = parameters, scoring = scorer, verbose = 2)
+    return cv
 
    
 def get_eval_metrics(actual, predicted, col_names):
@@ -134,9 +133,9 @@ def get_eval_metrics(actual, predicted, col_names):
     
     # Calculate evaluation metrics for each set of labels
     for i in range(len(col_names)):
-        accuracy = accuracy_score(actual[:, i], predicted[:, i])
-        precision = precision_score(actual[:, i], predicted[:, i])
-        recall = recall_score(actual[:, i], predicted[:, i])
+        accuracy = accuracy_score(actual[:, i], predicted[:, i],average='micro')
+        precision = precision_score(actual[:, i], predicted[:, i],average='micro')
+        recall = recall_score(actual[:, i], predicted[:, i],average='micro')
         f1 = f1_score(actual[:, i], predicted[:, i])
         
         metrics.append([accuracy, precision, recall, f1])
